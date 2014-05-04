43da39511a0536fd20968af74d6b8215bf3350d5
Taiki ONO <taiks.4559@gmail.com>
2014-05-04 20:32:15 +0900

    Add activesupport

--- lib/datapipes.rb
+++ lib/datapipes.rb
@@ -1,9 +1,9 @@
+require 'active_support/all'
 require 'datapipes/source'
 require 'datapipes/tube'
 require 'datapipes/sink'
 
 require 'datapipes/version'
-
 
 module Datapipes
   # Your code goes here...
