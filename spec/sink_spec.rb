47ed4b45bb0a7c810267085fc268e1f4943786ff
Taiki ONO <taiks.4559@gmail.com>
2014-05-06 03:54:54 +0900

    Add more specs fro sink composability

--- spec/sink_spec.rb
+++ spec/sink_spec.rb
@@ -2,40 +2,70 @@ require 'spec_helper'
 
 describe Datapipes::Sink do
   describe 'composable' do
-    subject { sink_a + sink_b + sink_c }
+    context 'with 3 sinks' do
+      subject { sink_a + sink_b + sink_c }
 
-    it 'composes' do
-      subject.run_all(5)
-      expect(list).to eq [8, 20, 6]
+      it 'composes' do
+        subject.run_all(5)
+        expect(list).to eq [8, 20, 6]
+      end
     end
-  end
 
-  let(:list) { [] }
+    context 'with 4 sinks' do
+      subject { sink_a + sink_b + sink_c + sink_d }
 
-  let(:sink_a) do
-    list_ = list
-    Class.new(Datapipes::Sink) do
-      define_method(:run) do |data|
-        list_ << (data + 3)
+      it 'composes' do
+        subject.run_all(5)
+        expect(list).to eq [8, 20, 6, 50]
       end
-    end.new
-  end
+    end
 
-  let(:sink_b) do
-    list_ = list
-    Class.new(Datapipes::Sink) do
-      define_method(:run) do |data|
-        list_ << (data * 4)
-      end
-    end.new
-  end
+    describe 'associative law' do
+      let(:a) { (sink_a + sink_b) + sink_c }
+      let(:b) { sink_a + (sink_b + sink_c) }
 
-  let(:sink_c) do
-    list_ = list
-    Class.new(Datapipes::Sink) do
-      define_method(:run) do |data|
-        list_ << (data + 1)
+      it 'keeps' do
+        expect(a.run_all(3)).to eq b.run_all(3)
       end
-    end.new
+    end
+
+    let(:list) { [] }
+
+    let(:sink_a) do
+      list_ = list
+      Class.new(Datapipes::Sink) do
+        define_method(:run) do |data|
+          list_ << (data + 3)
+        end
+      end.new
+    end
+
+    let(:sink_b) do
+      list_ = list
+      Class.new(Datapipes::Sink) do
+        define_method(:run) do |data|
+          list_ << (data * 4)
+        end
+      end.new
+    end
+
+    let(:sink_c) do
+      list_ = list
+      Class.new(Datapipes::Sink) do
+        define_method(:run) do |data|
+          list_ << (data + 1)
+        end
+      end.new
+    end
+
+    let(:sink_d) do
+      list_ = list
+      Class.new(Datapipes::Sink) do
+        define_method(:run) do |data|
+          list_ << (data * 10)
+        end
+      end.new
+    end
+
   end
 end
