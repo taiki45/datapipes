816233f9d61dcd4e2b9d7edba5e0be681e5c328d
Taiki ONO <taiks.4559@gmail.com>
2014-05-06 06:18:35 +0900

    Add coveralls configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
 require 'pry'
 require 'stringio'
 
+require 'coveralls'
+require 'simplecov'
+Coveralls.wear!
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter 'spec'
+end
+
 $: << File.expand_path('../../lib', __FILE__)
 require 'datapipes'
