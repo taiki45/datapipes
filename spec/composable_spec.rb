d096698136ba8a226a3b87e140beb2441f08994e
Taiki ONO <taiks.4559@gmail.com>
2014-05-06 01:57:19 +0900

    Move composable spec to tube spec

--- spec/composable_spec.rb
+++ spec/composable_spec.rb
@@ -1,35 +1,4 @@
 require 'spec_helper'
 
 describe Datapipes::Composable do
-  context 'with tube' do
-    let(:tube_a) do
-      Class.new(Datapipes::Tube) do
-        def run(data)
-          data + 2
-        end
-      end.new
-    end
-
-    let(:tube_b) do
-      Class.new(Datapipes::Tube) do
-        def run(data)
-          data * 3
-        end
-      end.new
-    end
-
-    let(:tube_c) do
-      Class.new(Datapipes::Tube) do
-        def run(data)
-          data + 4
-        end
-      end.new
-    end
-
-    subject { tube_a >> tube_b >> tube_c }
-
-    it 'generates new tube' do
-      expect(subject.run(4)).to eq 22
-    end
-  end
 end
---
+++ spec/tube_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe Datapipes::Tube do
+  describe 'composable' do
+    let(:tube_a) do
+      Class.new(Datapipes::Tube) do
+        def run(data)
+          data + 2
+        end
+      end.new
+    end
+
+    let(:tube_b) do
+      Class.new(Datapipes::Tube) do
+        def run(data)
+          data * 3
+        end
+      end.new
+    end
+
+    let(:tube_c) do
+      Class.new(Datapipes::Tube) do
+        def run(data)
+          data + 4
+        end
+      end.new
+    end
+
+    subject { tube_a >> tube_b >> tube_c }
+
+    it 'composes' do
+      expect(subject.run(4)).to eq 22
+    end
+  end
+
+end
