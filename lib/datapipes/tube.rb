f1413d495fc1c7378e64aaa32d2f2c7b0e09fa5f
Taiki ONO <taiks.4559@gmail.com>
2014-05-06 19:16:28 +0900

    Add tube doc

--- lib/datapipes/tube.rb
+++ lib/datapipes/tube.rb
@@ -3,6 +3,9 @@ class Datapipes
   #
   # Build your own tube logic in `run` method.
   class Tube
+    # _>>_ is used to compose tubes. See usage in examples.
+    #
+    # Tube composition satisfies associative law. See more in spec.
     def >>(op2)
       op1 = self
       Tube.new.tap do |o|
@@ -12,6 +15,24 @@ class Datapipes
       end
     end
 
+    # Override this in sub class.
+    #
+    # _run_ recieves any data, so you have to ignore
+    # unexpected data.
+    #
+    #   def run(data)
+    #     if accept? data
+    #       [data, data, data]
+    #     else
+    #       data
+    #     end
+    #   end
+    #
+    #    def accept?(data)
+    #      data.is_a? Integer and data > 3
+    #    end
+    #
+    # Don't forget to return data in else clause.
     def run(data)
       data
     end
