6dd21abd5c0071a12100dc0904232a8f2bb986d7
Taiki ONO <taiks.4559@gmail.com>
2014-05-06 19:07:27 +0900

    Add doc to pipe

--- lib/datapipes/pipe.rb
+++ lib/datapipes/pipe.rb
@@ -1,13 +1,26 @@
 class Datapipes
+  # Pipe is not only data pipeline but handling asynchronous.
+  #
+  # You can make your own pipe with database or something, but
+  # becareful this object used in multi thread.
+  #
+  # If you make your own, you can override _initialize_, because
+  # this is not used in internal.
+  #
+  # Then supply _recieve_ and _pull_. _pull_ must cause thread blocking
+  # when it is empty.
   class Pipe
+    # You can override and don't need to call super in sub class.
     def initialize
       @queue ||= Queue.new
     end
 
+    # Emit data to pipe.
     def recieve(data)
       @queue.enq data
     end
 
+    # _pull_ must cause thread blocking when it is empty.
     def pull
       @queue.deq
     end
