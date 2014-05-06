dfc55c1d3846ab2912130f8593b0c207d68c9e31
Taiki ONO <taiks.4559@gmail.com>
2014-05-06 19:28:34 +0900

    Add composable doc

--- lib/datapipes/composable.rb
+++ lib/datapipes/composable.rb
@@ -2,6 +2,8 @@ class Datapipes
   module Composable
     attr_accessor :accumulated
 
+    # Source and Sink can't composed as function composition.
+    # So accumulates composed objects internaly, then use them later.
     def +(op2)
       op1 = self
       op1_acc = (op1.accumulated || [op1])
