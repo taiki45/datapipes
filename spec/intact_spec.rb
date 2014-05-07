5eb7fd16779f26a743fd152c1d59a55ad27f7219
Taiki ONO <taiks.4559@gmail.com>
2014-05-07 22:19:25 +0900

    Add more intact objects spec

--- spec/intact_spec.rb
+++ spec/intact_spec.rb
@@ -1,11 +1,29 @@
 require 'spec_helper'
 
 describe 'intact objects' do
-  let(:datapipe) do
-    Datapipes.new
+  context 'with intact all' do
+    let(:datapipe) do
+      Datapipes.new
+    end
+
+    it 'runs without any errors' do
+      expect { datapipe.run_resource }.not_to raise_error
+    end
   end
 
-  it 'runs but occurs nothing' do
-    expect { datapipe.run_resource }.not_to raise_error
+  context 'with intact tube and sink' do
+    let(:source) do
+      Class.new(Datapipes::Source) do
+        def run
+          10.times {|i| produce(i) }
+        end
+      end.new
+    end
+
+    let(:datapipe) { Datapipes.new(source: source) }
+
+    it 'runs without any errors' do
+      expect { datapipe.run_resource }.not_to raise_error
+    end
   end
 end
